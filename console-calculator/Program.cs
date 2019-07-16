9de36572800803b0cf49e89a9ab494a96070945b
Marina <marina-laym@mail.ru>
2019-07-16 11:42:17 +0500

    create gitignore

---
+++ console-calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_calculator {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Введите ваше имя:");
+
+            string sName = Console.ReadLine();
+
+            Console.WriteLine("Привет, " + sName);
+
+            Console.WriteLine("Нажмите ENTER что бы выйти из программы");
+
+            Console.Read();
+        }
+    }
+}
